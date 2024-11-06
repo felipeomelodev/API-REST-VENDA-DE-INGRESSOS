2eeb5d22e1bd9de0ca180adeae72d56af3aa8001
felipeomelo <felipe.o.melo@hotmail.com>
2024-11-06 10:51:12 -0300

    Configuração do servidor Express em app.ts e middlewere de JSON

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
-import "dotenv/config";
-import express, { Application } from "express";
-import rotas from "./rotas";
+import "dotenv/config"
+import express, { Application } from "express"
+import rotas from "./rotas"
 
-const app: Application = express();
+const app: Application = express()
 
-app.use(express.json());
-app.use(rotas);
+app.use(express.json())
+app.use(rotas)
 
-export default app;
+export default app
